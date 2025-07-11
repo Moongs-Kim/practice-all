0053adad237c24a4a345452492acfe3010737931
moongs <jinee5050@naver.com>
2025-07-11 23:15:41 +0900

    java mid1 Immutable

---
+++ src/javamid/lang/immutable/ImmutableAddress.java
@@ -0,0 +1,21 @@
+package javamid.lang.immutable;
+
+public class ImmutableAddress {
+
+    private final String value;
+
+    public ImmutableAddress(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "ImmutableAddress{" +
+                "value='" + value + '\'' +
+                '}';
+    }
+}
