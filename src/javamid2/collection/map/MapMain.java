a1b79c43ff201df6dbc5656340f6be0035694312
moongs <jinee5050@naver.com>
2025-07-31 13:46:47 +0900

    java mid2 Map

---
+++ src/javamid2/collection/map/MapMain.java
@@ -0,0 +1,25 @@
+package javamid2.collection.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapMain {
+
+    public static void main(String[] args) {
+        Map<String, Integer> studentMap = new HashMap<>();
+
+        studentMap.put("studentA", 50);
+        System.out.println("studentMap = " + studentMap);
+
+        // 학생이 없는 경우에만 추가1
+        if (!studentMap.containsKey("studentA")) {
+            studentMap.put("studentA", 100);
+        }
+        System.out.println("studentMap = " + studentMap);
+
+        // 학생이 없는 경우에만 추가2
+        studentMap.putIfAbsent("studentA", 100);
+        studentMap.putIfAbsent("studentB", 100);
+        System.out.println("studentMap = " + studentMap);
+    }
+}
