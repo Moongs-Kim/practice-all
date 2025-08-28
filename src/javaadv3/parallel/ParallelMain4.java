8ac5e8ba462df3d8ab5c14ae2aad034ff14d7296
moongs <jinee5050@naver.com>
2025-08-28 18:30:22 +0900

    java adv3 parallel stream

---
+++ src/javaadv3/parallel/ParallelMain4.java
@@ -0,0 +1,26 @@
+package javaadv3.parallel;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.stream.IntStream;
+
+import static javaadv3.util.MyLogger.log;
+
+public class ParallelMain4 {
+
+    public static void main(String[] args) {
+        int processorsCount = Runtime.getRuntime().availableProcessors();
+        ForkJoinPool commonPool = ForkJoinPool.commonPool();
+        log("processorCount = " + processorsCount + ", commonPool = " + commonPool.getParallelism());
+
+        long startTime = System.currentTimeMillis();
+
+        int sum = IntStream.rangeClosed(1, 8)
+                .parallel()
+                .map(HeavyJob::heavyTask)
+                .reduce(0, (a, b) -> a + b);
+
+        long endTime = System.currentTimeMillis();
+        log("time: " + (endTime - startTime) + "ms, sum: " + sum);
+
+    }
+}
---
+++ src/javaadv3/parallel/forkjoin/CompletableFutureMain.java
@@ -0,0 +1,19 @@
+package javaadv3.parallel.forkjoin;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static javaadv3.util.MyLogger.log;
+
+public class CompletableFutureMain {
+
+    public static void main(String[] args) {
+        CompletableFuture.runAsync(() -> log("Fork/Join"));
+
+        ExecutorService es = Executors.newFixedThreadPool(100);
+        CompletableFuture.runAsync(() -> log("Custom Pool"), es);
+        es.close();
+    }
+
+}
